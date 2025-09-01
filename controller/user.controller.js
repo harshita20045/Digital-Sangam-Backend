f715c10afc6859fe5bd0debb2e004b9d254cf6be
Harshita <baghelharshita2005@gmail.com>
2025-09-01 11:22:54 +0530

    user exist correct

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -288,7 +288,7 @@ export const getAllUserByEmailId = async (req, res) => {
     if (user) {
       res.status(200).json(user);
     } else {
-      res.status(404).json({ message: "User not found" }); 
+      res.status(200).json({ message: "User not found", user: null });
     }
   } catch (err) {
     res.status(500).json({ message: err.message }); 
